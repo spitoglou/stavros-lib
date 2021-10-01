3cd5517c2608ada0a1c36fd21f4e8883ef8adeec
Stavros Pitoglou <s.pitoglou@csl.gr>
2021-10-02 02:22:12 +0300

    v0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='stavroslib',
-      version='0.1',
+      version='0.2',
       description='My common libraries',
       url='https://github.com/spitoglou/stavros-lib',
       author='Stavros Pitoglou',
